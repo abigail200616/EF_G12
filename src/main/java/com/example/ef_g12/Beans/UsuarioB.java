fecd7e5342c999de5a854c61219ed97bb75c03c3
abigail200616 <abigail.ramirez@pucp.edu.pe>
2022-11-29 13:24:46 -0500

    abigail UsuarioB

--- src/main/java/com/example/ef_g12/Beans/UsuarioB.java
+++ src/main/java/com/example/ef_g12/Beans/UsuarioB.java
@@ -1,4 +1,59 @@
 package com.example.ef_g12.Beans;
 
 public class UsuarioB {
+
+    private int idUsuario;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String contraseña;
+    private String tipo;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
