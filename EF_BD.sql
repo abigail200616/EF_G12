c41ef673d82165d6c05e03af1db9bb640df818a3
abigail200616 <abigail.ramirez@pucp.edu.pe>
2022-11-29 13:20:56 -0500

    abigail

---
+++ EF_BD.sql
@@ -0,0 +1,106 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema EF_BD
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema EF_BD
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `EF_BD` DEFAULT CHARACTER SET utf8 ;
+USE `EF_BD` ;
+
+-- -----------------------------------------------------
+-- Table `EF_BD`.`Usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `EF_BD`.`Usuario` (
+  `idUsuario` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NULL,
+  `apellido` VARCHAR(45) NULL,
+  `correo` VARCHAR(100) NULL,
+  `contrasena` VARCHAR(512) NULL,
+  `tipo` VARCHAR(45) NULL,
+  PRIMARY KEY (`idUsuario`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `EF_BD`.`Impresion`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `EF_BD`.`Impresion` (
+  `idImpresion` INT NOT NULL,
+  `densidad` INT NULL,
+  `grosor` VARCHAR(45) NULL,
+  `altura` FLOAT NULL,
+  `material` VARCHAR(45) NULL,
+  `url` VARCHAR(200) NULL,
+  `autoservicio` TINYINT NULL,
+  `costo` FLOAT NULL,
+  `tiempo` FLOAT NULL,
+  `fecha` DATE NULL,
+  `table1_idUsuario` INT NOT NULL,
+  PRIMARY KEY (`idImpresion`),
+  INDEX `fk_Impresion_table1_idx` (`table1_idUsuario` ASC) VISIBLE,
+  CONSTRAINT `fk_Impresion_table1`
+    FOREIGN KEY (`table1_idUsuario`)
+    REFERENCES `EF_BD`.`Usuario` (`idUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `EF_BD`.`Laser`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `EF_BD`.`Laser` (
+  `idCorte` INT NOT NULL,
+  `grosor` VARCHAR(45) NULL,
+  `color` VARCHAR(45) NULL,
+  `material` VARCHAR(45) NULL,
+  `dimensiones` FLOAT NULL,
+  `url` VARCHAR(200) NULL,
+  `costo` FLOAT NULL,
+  `tiempo` FLOAT NULL,
+  `fecha` DATE NULL,
+  `table1_idUsuario` INT NOT NULL,
+  PRIMARY KEY (`idCorte`),
+  INDEX `fk_Laser_table11_idx` (`table1_idUsuario` ASC) VISIBLE,
+  CONSTRAINT `fk_Laser_table11`
+    FOREIGN KEY (`table1_idUsuario`)
+    REFERENCES `EF_BD`.`Usuario` (`idUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `EF_BD`.`Escaneo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `EF_BD`.`Escaneo` (
+  `idEscaneo` INT NOT NULL,
+  `largo` FLOAT NULL,
+  `ancho` FLOAT NULL,
+  `altura` FLOAT NULL,
+  `resolucion` VARCHAR(45) NULL,
+  `costo` FLOAT NULL,
+  `tiempo` FLOAT NULL,
+  `fecha` DATE NULL,
+  `Escaneocol` VARCHAR(45) NULL,
+  `table1_idUsuario` INT NOT NULL,
+  PRIMARY KEY (`idEscaneo`),
+  INDEX `fk_Escaneo_table11_idx` (`table1_idUsuario` ASC) VISIBLE,
+  CONSTRAINT `fk_Escaneo_table11`
+    FOREIGN KEY (`table1_idUsuario`)
+    REFERENCES `EF_BD`.`Usuario` (`idUsuario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
